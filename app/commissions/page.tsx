df2f5f55b91001e741bd68e39f1e2ee96f3b7994
Plexdi <senpai200798@gmail.com>
2025-11-06 00:44:43 +0000

    Update fetch URL in CommissionsPage to use production endpoint

--- app/commissions/page.tsx
+++ app/commissions/page.tsx
@@ -26,7 +26,7 @@ export default function CommissionsPage() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:8080/commissions", {
+      const res = await fetch("https://backend-8qjc.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
